6b4ccf388f1ccec5c8ec71776139fd015d6a8f02
Yuri Konev <crazy_clown93@mail.ru>
2024-07-20 21:40:48 +0300

    add program

--- Program_Final_Test/Program.cs
+++ Program_Final_Test/Program.cs
@@ -8,71 +8,26 @@
 //[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 //[“Russia”, “Denmark”, “Kazan”] → []
 
-//Console.WriteLine("Введите элементы массива, через пробел:");
-//string input = Console.ReadLine();
-//string[] array = input.Split(' ');
+Console.WriteLine("Введите элементы массива, через пробел:");
+string input = Console.ReadLine();
+string[] array = input.Split(' ');
 
 
-//string[] newArray = new string[array.Length]; 
-//int newIndex = 0;
+string[] newArray = new string[array.Length];
+int newIndex = 0;
 
 
-//for (int i = 0; i < array.Length; i++) 
-//{
-//    if (array[i].Length <= 3)
-//    {
-//        newArray[newIndex] = array[i];
-//        newIndex++;
-//    }
-//}
-//Console.WriteLine("новый массив:");
-//for (int i = 0; i < newIndex; i++)
-//{
-//    Console.Write(newArray[i] + " ");
-//}
-
-using System;
-
-class Program
+for (int i = 0; i < array.Length; i++)
 {
-    static void Main(string[] args)
+    if (array[i].Length <= 3)
     {
-        Console.WriteLine("Введите строки через пробел:");
-
-        // Ввод строк с клавиатуры
-        string[] strings = Console.ReadLine().Split();
-        Console.Write("Исходный массив: ");
-        foreach (string s in strings)
-        {
-            Console.Write($"{s} ");
-        }
-        Console.WriteLine();
-
-        // Обработка массива и вывод результата
-        int count = 0;
-        for (int i = 0; i < strings.Length; i++)
-        {
-            if (strings[i].Length <= 3)
-            {
-                count++;
-            }
-        }
-
-        string[] result = new string[count];
-        count = 0;
-        for (int i = 0; i < strings.Length; i++)
-        {
-            if (strings[i].Length <= 3)
-            {
-                result[count] = strings[i];
-                count++;
-            }
-        }
-
-        Console.Write($"Новый массив: ");
-        for (int i = 0; i < result.Length; i++)
-        {
-            Console.Write($"{result[i]} ");
-        }
+        newArray[newIndex] = array[i];
+        newIndex++;
     }
 }
+Console.WriteLine("новый массив:");
+for (int i = 0; i < newIndex; i++)
+{
+    Console.Write(newArray[i] + " ");
+}
+
